4e08a16849ddab94334f70d5bada9117d55d6b67
icis-04 <chidexiwuaku@gmail.com>
2022-06-06 14:20:19 -0700

    reversed list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for i in range(0, len(my_list) - 1):
-        i = idx
-        if i < 0:
-            return my_list
-        elif i >= len(my_list):
-            return my_list
-        elif i == idx:
-            return my_list.insert(idx, element)
-    return
+    if idx < 0:
+         print(my_list)
+    elif idx >= len(my_list):
+        print(my_list)
+    else:
+        print(my_list.insert(idx, element))
