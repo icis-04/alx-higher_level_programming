198179e472530810e765bed60952d97523fa51d0
icis-04 <chidexiwuaku@gmail.com>
2022-06-10 18:38:20 -0700

    done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,3 @@
-#!/usr.bin/python3
+#!/usr/bin/python3
 def best_score(a_dictionary):
-    ls = a_dictionary.items()
-    max_ls = max(ls)
-    if a_dictionary == None:
-        return None
-    else:
-        for i in a_dictionary.keys():
-            if a_dictionary[i] == max_ls:
-                return i
-    return
+    return (max(a_dictionary, key=a_dictionary.get) if a_dictionary else None)i
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for i in a_dictionary:
-        print(str(i)+":", a_dictionary[i] * 2)
-    return
+    return {k: v * 2 for k, v in a_dictionary.items()}
