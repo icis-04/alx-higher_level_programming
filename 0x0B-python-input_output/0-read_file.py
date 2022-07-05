f23dfe25620f9528ebc3987fb24c8659777975f0
icis-04 <chidexiwuaku@gmail.com>
2022-07-05 12:47:26 -0700

    comments

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+    0-read_file: read_file()
+"""
 def read_file(filename=""):
+    """
+        Returns string in a file.
+        Args:
+            filename(object): string.
+    """
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
     print(read_data, end="")
