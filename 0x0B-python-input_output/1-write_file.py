2068376e2dabd4de8f46628a897aea48920c664f
icis-04 <chidexiwuaku@gmail.com>
2022-07-05 13:48:12 -0700

    python is cool

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    1-write_file: write_file()
+"""
+def write_file(filename="", text=""):
+    """
+        Returns the text as a string in the file
+        Args:
+            filename(object): string
+            text(object): string
+    """
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(text)
+    return f
