dae25f1029d4d66de41de364f45983b7d0682931
icis-04 <chidexiwuaku@gmail.com>
2022-07-04 21:36:47 -0700

    sort list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Mylist(list):
+    def __init__():
+        list.__init__()
+    def print_sorted(self):
+        return sorted(self)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+pyhton inheritance module in alx
