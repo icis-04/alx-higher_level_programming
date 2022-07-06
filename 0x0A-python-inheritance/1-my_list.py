af67d726649a87af9efe74399828da2a38d8de75
icis-04 <chidexiwuaku@gmail.com>
2022-07-06 10:30:03 -0700

    sort

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,17 +6,25 @@
 
 class MyList(list):
     """
-        This class inherits from list.
+        MyList is a class that inherits from the list object
         Attributes:
+            __init__: initializes the class instance
         Methods:
-            print_sorted - prints the list in ascending order
+            print_sorted: prints a list in ascending format
+            Returns: a sorted list in ascending form
     """
+    def __init__(self):
+        """
+            __init__ initialises the class
+            Args:
+                self: the instance referring to the class
+        """
+        list.__init__(self)
     def print_sorted(self):
         """
-           prints a list in ascending order.
+            print_sorted prints a sorted list
+            Args:
+                self: argument that refers to the class
+                Returns: sorted list
         """
-        print(sorted(self))List(list):
-    def __init__():
-        list.__init__()
-    def print_sorted(self):
-        return sorted(self)
+        return self.sort()
