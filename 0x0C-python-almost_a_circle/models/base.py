070777a2f60e35ec1f84548d34c9e16d468eb264
icis-04 <chidexiwuaku@gmail.com>
2022-07-13 22:52:41 -0700

    documentations

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 class Base:
+    """Represent the base model.
+    Represents the "base" for all other classes in project 0x0C*.
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
     """
-        base creates a base model
-        Attributes:
-            __nb_objects: private variable
-    """
+
     __nb_objects = 0
+
     def __init__(self, id=None):
-        """Initializes the instance
+        """Initialize a new Base.
         Args:
-            id (int): the id of the new base
+            id (int): The identity of the new Base.
         """
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
-        
-        
+            self.id = Base.__nb_objects        
