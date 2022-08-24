f5705f20130d631a730ae86c7bc21b6abb3d3998
icis-04 <chidexiwuaku@gmail.com>
2022-08-24 11:44:55 -0700

    safe division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+def safe_print_division(a, b):
+    quotient = None
+    try:
+        quotient = a / b
+        print("Inside result: {}".format(quotient))
+    except:
+        print("Inside result: {}".format(quotient))
+    finally:
+        return quotient
