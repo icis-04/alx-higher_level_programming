e2bfe9e06d252a9e1b29a28d1a3a6121cd6dbf41
icis-04 <chidexiwuaku@gmail.com>
2022-09-06 04:50:17 +0000

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,4 +1,16 @@
 #!/usr/bin/node
 // calculates factorial of a number
 function factorial(a){
-if 
+if (a == 0){
+return 1;
+}else{
+return a * factorial(a - 1);
+}
+}
+const process = require('process');
+if (process.argv.length === 2){
+console.log(1);
+}else{
+console.log(factorial(parseInt(process.argv[2])));
+}
+ 
