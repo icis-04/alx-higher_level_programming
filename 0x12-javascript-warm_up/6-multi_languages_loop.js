86ba98d23f07b0bfcfbe0206909e0c2eb64f8245
icis-04 <chidexiwuaku@gmail.com>
2022-09-06 02:24:16 +0000

    loops

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// printing with loops
+let i = 0;
+while (i < 3){
+console.log('C is fun');
+i++;
+console.log('Python is cool');
+i++;
+console.log('JavaScript is amazing');
+i++;
+}
