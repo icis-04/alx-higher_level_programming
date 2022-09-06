d74f869c4a976691c3b595052d570b852d2b6fe7
icis-04 <chidexiwuaku@gmail.com>
2022-09-06 02:55:26 +0000

    i love c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,9 +4,9 @@ const process = require('process');
 let n = parseInt(process.argv[2]);
 let m = Number.isInteger(n);
 if (m === false){
-	console.log('Missing number of occurrences');
+console.log('Missing number of occurrences');
 }else{
-	for (let i = 0; i < n; i++){
-		console.log('C is fun');
-	}
+for (let i = 0; i < n; i++){
+console.log('C is fun');
+}
 }
