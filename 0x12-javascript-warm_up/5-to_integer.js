b9aabf62c7a594b280cdbc7be157e21c69b1a4d5
icis-04 <chidexiwuaku@gmail.com>
2022-09-06 02:13:24 +0000

    string to number

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// check if the first argument passed is a number
+const process = require('process');
+if (Number.isInteger(parseInt(process.argv[2])) === false){
+	console.log('Not a number');
+}else{
+	console.log(parseInt(process.argv[2]));
+} 
