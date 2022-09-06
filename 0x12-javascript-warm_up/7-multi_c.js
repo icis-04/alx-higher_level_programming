542a71be6a0e8e0db5db9655e6c343be819d6d36
icis-04 <chidexiwuaku@gmail.com>
2022-09-06 02:50:01 +0000

    c is fun

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// looping with arguments
+const process = require('process');
+let n = parseInt(process.argv[2]);
+let m = Number.isInteger(n);
+if (m === false){
+	console.log('Missing number of occurrences');
+}else{
+	for (let i = 0; i < n; i++){
+		console.log('C is fun');
+	}
+}
