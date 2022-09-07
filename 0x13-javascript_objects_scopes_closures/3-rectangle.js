28119a58fca93a2f6532523102d221ce1fa1e045
icis-04 <chidexiwuaku@gmail.com>
2022-09-07 04:00:43 +0000

    still on rectangles

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+constructor(w, h){
+if (w > 0 && h > 0) {
+this.width = w;
+this.height = h;
+}
+};
+print() {
+let i = 0;
+let j = 'X';
+while (i < this.height) {
+console.log(j.repeat(this.width));
+i++;
+}
+}
+};
