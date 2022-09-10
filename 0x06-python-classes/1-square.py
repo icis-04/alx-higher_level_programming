d78444ffcc0402daa7e71d5612a8be6521b29a13
icis-04 <chidexiwuaku@gmail.com>
2022-09-10 11:57:11 -0700

    square things

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" 1-square.py - defines and intaializes a square class"""
 class Square():
+    """ the Square class """
     def __init__(self, size):
+        """initializes the size"""
         self.__size = size
