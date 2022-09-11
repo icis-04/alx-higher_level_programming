0cf6ada1d22b1723a50fa8455787b15645bd07e3
icis-04 <chidexiwuaku@gmail.com>
2022-09-11 22:58:46 +0000

    states state

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa
+"""script that lists all states with a name starting with N
+(upper N) from the database hbtn_0e_0_usa
 """
 from sys import argv
 import MySQLdb
@@ -9,8 +10,9 @@ if __name__ == '__main__':
     user, password, database = argv[1], argv[2], argv[3]
     db = MySQLdb.connect(host="localhost", user=user,
                          passwd=password, db=database)
-    db.query("SELECT * FROM states ORDER BY id")
-    r = db.store_result()
-    t = r.fetch_row(maxrows=0)
-    for i in t:
+    db = db.cursor()
+    db.execute("""SELECT * FROM states
+    WHERE REGEXP_LIKE(name, '^N', 'c') ORDER BY id""")
+    r = db.fetchall()
+    for i in r:
         print(i)
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""script that lists all states from the database hbtn_0e_0_usa
+"""script that lists all cities from the database hbtn_0e_4_usa
 """
 from sys import argv
 import MySQLdb
@@ -9,8 +9,9 @@ if __name__ == '__main__':
     user, password, database = argv[1], argv[2], argv[3]
     db = MySQLdb.connect(host="localhost", user=user,
                          passwd=password, db=database)
-    db.query("SELECT * FROM states ORDER BY id")
-    r = db.store_result()
-    t = r.fetch_row(maxrows=0)
-    for i in t:
+    db = db.cursor()
+    db.execute("""SELECT cities.id, cities.name, states.name FROM cities
+    JOIN states ON state_id=states.id ORDER BY cities.id""")
+    r = db.fetchall()
+    for i in r:
         print(i)
