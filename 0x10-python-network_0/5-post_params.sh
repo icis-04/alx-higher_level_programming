d337a78148661f442b57f3c1affc0d45e9b1bc15
icis-04 <chidexiwuaku@gmail.com>
2022-09-22 21:36:52 -0700

    updated 5-post_params.sh file

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script that sends a DELETE request to the URL passed and display body of response
-curl -s -X "POST" "$1" -d "email=hr@holbertonschool.com&subject=I will always
+curl -s -X "POST" "$1" -d "email=test@gmail.com&subject=I will always
 be here for PLD"
