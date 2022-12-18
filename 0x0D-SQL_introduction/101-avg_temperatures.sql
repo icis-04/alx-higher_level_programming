9365adc3c50267513c58d0ab5ad3d9145eb6dd8b
icis-04 <chidexiwuaku@gmail.com>
2022-12-18 01:51:47 -0800

    updated file

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,5 @@
 -- Temperature average
+IMPORT TABLE
 SELECT city, AVG(value) AS 'avg_temp'
 FROM temperature
 GROUP BY city
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,10 +1,10 @@
 -- creates a second table with multiple descriptions and record 
-CREATE TABLE IF NOT EXISTS `second_table`(
-	`id` INT,
-	`name` VARCHAR(256),
-	`score` INT
+CREATE TABLE IF NOT EXISTS second_table(
+	id INT,
+	name VARCHAR(256),
+	score INT
 );
-INSERT INTO `second_table`(`id`, `name` ,`score`) VALUES('1',"John", '10');
-INSERT INTO `second_table`(`id`, `name` ,`score`) VALUES('2',"Alex", '3');
-INSERT INTO `second_table`(`id`, `name` ,`score`) VALUES('1',"Bob", '14');
-INSERT INTO `second_table`(`id`, `name` ,`score`) VALUES('1',"George", '8');
+INSERT INTO second_table(id, name ,score) VALUES(1,"John", 10);
+INSERT INTO second_table(id, name ,score) VALUES(2,"Alex", 3);
+INSERT INTO second_table(id, name ,score) VALUES(1,"Bob", 14);
+INSERT INTO second_table(id, name ,score) VALUES(1,"George", 8);
