bbcea44f8ad7645346da1965a1c66a3746ae809b
icis-04 <chidexiwuaku@gmail.com>
2022-12-17 22:02:09 -0800

    update to the 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- creates a second table with multiple descriptions and record 
-CREATE TABLE IF NOT EXISTS second_table(
-	id INT,
-	name VARCHAR(256),
-	score INT
+CREATE TABLE IF NOT EXISTS `second_table`(
+	`id` INT,
+	`name` VARCHAR(256),
+	`score` INT
 );
 INSERT INTO `second_table`(`id`, `name` ,`score`) VALUES('1',"John", '10');
 INSERT INTO `second_table`(`id`, `name` ,`score`) VALUES('2',"Alex", '3');
